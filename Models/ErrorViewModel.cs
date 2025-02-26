0a9c51973b3255bfabda0f5cda247ae429f66f01
Elandré van der Merwe <u21471674@up.ac.za>
2025-02-26 11:45:00 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace u21471674_Practical1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
